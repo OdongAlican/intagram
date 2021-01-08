369601c77a40a3c5fde11407990220f06136054f
OdongAlican <sandieo.2020@gmail.com>
2021-01-08 21:28:47 +0300

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer-section">
+    <div className="upper-footer-section">
+      <div className="about-us-footer">
+        <h6>About</h6>
+      </div>
+      <div className="support-footer">
+        <h6>Blog</h6>
+      </div>
+      <div className="blog-footer">
+        <h6>Jobs</h6>
+      </div>
+      <div className="press-footer">
+        <h6>Help</h6>
+      </div>
+      <div className="api-footer">
+        <h6>API</h6>
+      </div>
+      <div className="job-footer">
+        <h6>Privacy</h6>
+      </div>
+      <div className="privacy-footer">
+        <h6>Terms</h6>
+      </div>
+      <div className="terms-footer">
+        <h6>Top Accounts</h6>
+      </div>
+      <div className="directory-footer">
+        <h6>Hashtags</h6>
+      </div>
+      <div className="language-footer">
+        <h6>Locations</h6>
+      </div>
+    </div>
+    <div className="upper-footer-section">
+      <div className="privacy-footer">
+        <h6>Beauty</h6>
+      </div>
+      <div className="terms-footer">
+        <h6>Dance &amp; Performance</h6>
+      </div>
+      <div className="directory-footer">
+        <h6>Fitness</h6>
+      </div>
+      <div className="language-footer">
+        <h6>Food &amp; Drink</h6>
+      </div>
+      <div className="terms-footer">
+        <h6>Home &amp; Garden</h6>
+      </div>
+      <div className="directory-footer">
+        <h6>Music</h6>
+      </div>
+      <div className="language-footer">
+        <h6>Visual Arts</h6>
+      </div>
+    </div>
+    <div className="upper-footer-section">
+      <div className="privacy-footer mt-2">
+        <h6>&copy;2021 Instagram from Alican</h6>
+      </div>
+    </div>
+  </div>
+);
+
+export default Footer;
