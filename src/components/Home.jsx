24a1ba5c3d70a106b3c0bdf054802348cbef2aa4
OdongAlican <sandieo.2020@gmail.com>
2021-01-14 05:51:28 +0300

    update Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Navbar from './Navbar';
 
-const Home = () => (
-  <div className="home-section">
-    <p>Home Section</p>
-  </div>
-);
-
+const Home = () => {
+  console.log(localStorage.userId);
+  console.log(localStorage.token, 'token inner');
+  return (
+    <div className="home-section">
+      <Navbar />
+      <div className="main-inner-home-section bg-info">
+        <div className="home-main-photo-section">
+          <p>Home section</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Home;
