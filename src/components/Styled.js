cb0246b8773b3e86706870c71b0d6d1fb142b6ba
OdongAlican <sandieo.2020@gmail.com>
2021-01-31 11:42:42 +0300

    add styled component

---
+++ src/components/Styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 16px 0;
+    color: #fff;
+    margin: 0 15px;
+    font-size: 14em;
+`;
