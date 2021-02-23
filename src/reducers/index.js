6b50d1a50f0c25602259aad9e81efecf5276ffe8
OdongAlican <sandieo.2020@gmail.com>
2021-02-23 18:28:36 +0300

    add postReducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import userReducer from './userReducer';
+import postReducer from './postReducer';
 
 const rootReducer = combineReducers({
   authReducer,
   userReducer,
+  postReducer,
 });
 
 export default rootReducer;
