1309b0d8a717b8212b600bab0e416ea64f2d618d
OdongAlican <sandieo.2020@gmail.com>
2021-03-12 15:04:12 +0300

    add conversations route

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -6,6 +6,7 @@ import Signout from './auth/Signout';
 import Signup from './auth/Signup';
 import Home from './Home';
 import Profile from './Profile';
+import ConversationsList from './ConversationsList';
 
 const Router = () => (
   <div className="router-section">
@@ -13,6 +14,7 @@ const Router = () => (
       <Switch>
         <Route exact path="/" component={Signin} />
         <Route exact path="/signout" component={Signout} />
+        <Route exact path="/conversations" component={ConversationsList} />
         <Route exact path="/signup" component={Signup} />
         <PrivateRoute exact path="/home" component={Home} />
         <PrivateRoute path="/user/:id" component={Profile} />
