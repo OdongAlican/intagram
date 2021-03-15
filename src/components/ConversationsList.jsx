df2c7135a0e714159768a422f957e9c76a0f772a
OdongAlican <sandieo.2020@gmail.com>
2021-03-16 00:01:29 +0300

    update conversation list layout

--- src/components/ConversationsList.jsx
+++ src/components/ConversationsList.jsx
@@ -5,26 +5,38 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable no-unused-vars */
 
 import React from 'react';
 import { ActionCable } from 'react-actioncable-provider';
 import axios from 'axios';
 import { API_ROOT } from '../constants';
-import NewConversationForm from './NewConversationForm';
+// import NewConversationForm from './NewConversationForm';
 import MessagesArea from './MessagesArea';
 import Cable from './Cable';
 import { headers } from '../utils/api';
+import Profileimage from '../Images/icon.png';
+import NavBar from './Navbar';
 
 class ConversationsList extends React.Component {
-    state = {
+  constructor(props) {
+    super(props);
+    this.state = {
       conversations: [],
       activeConversation: null,
+      userId: null,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    componentDidMount = () => {
+    componentDidMount = async () => {
       try {
-        axios.get(`${API_ROOT}/conversations`, headers)
-          .then(resp => this.setState({ conversations: resp.data }));
+        const response = await axios.get(`${API_ROOT}/conversations`, headers);
+        this.setState({
+          conversations: response.data,
+          userId: localStorage.userId,
+        });
+        console.log(localStorage.token);
       } catch (error) {
         console.log(error);
       }
@@ -39,6 +51,8 @@ class ConversationsList extends React.Component {
       this.setState({
         conversations: [...this.state.conversations, conversation],
       });
+
+      this.handleClick = this.handleClick.bind(this);
     }
 
     handleReceivedMessage = response => {
@@ -53,30 +67,66 @@ class ConversationsList extends React.Component {
     }
 
     render = () => {
-      const { conversations, activeConversation } = this.state;
+      const { conversations, activeConversation, userId } = this.state;
       return (
-        <div className="conversationsList">
-          <ActionCable
-            channel={{ channel: 'ConversationsChannel' }}
-            onReceived={this.handleReceivedConversation}
-          />
-          {this.state.conversations.length ? (
-            <Cable
-              conversations={conversations}
-              handleReceivedMessage={this.handleReceivedMessage}
-            />
-          ) : null}
-          <h2>Conversations</h2>
-          <ul>{mapConversations(conversations, this.handleClick)}</ul>
-          <NewConversationForm />
-          {activeConversation ? (
-            <MessagesArea
-              conversation={findActiveConversation(
-                conversations,
-                activeConversation,
+        <div className="messaging-section">
+          <NavBar />
+          <div className="conversationsList-section">
+            <div className="left-conversation-section">
+              <ActionCable
+                channel={{ channel: 'ConversationsChannel' }}
+                onReceived={this.handleReceivedConversation}
+              />
+              {this.state.conversations.length ? (
+                <Cable
+                  conversations={conversations}
+                  handleReceivedMessage={this.handleReceivedMessage}
+                />
+              ) : null}
+              <div className="top-header-section">Conversations</div>
+              {
+            conversations
+              .filter(conversation => (
+                conversation.reciever_id === parseInt(this.state.userId, 10)
+                  || conversation.user_id === parseInt(this.state.userId, 10)))
+              .map(conversation => (
+                <div
+                  className="each-conversation"
+                  key={conversation.id}
+                  onClick={() => this.handleClick(conversation.id)}
+                >
+                  <div className="reciever-image">
+                    <img src={Profileimage} alt="profile" />
+                  </div>
+                  <div className="reciever-name">
+                    {conversation.title}
+                  </div>
+                </div>
+              ))
+          }
+              {/* <NewConversationForm /> */}
+            </div>
+            <div className="right-reservation-section">
+              {activeConversation ? (
+                <MessagesArea
+                  conversation={findActiveConversation(
+                    conversations,
+                    activeConversation,
+                  )}
+                />
+              ) : (
+                <div className="main-sample-image-sect">
+                  <div className="sample-image-section" />
+                  <div className="sample-message-section">
+                    Your Messages
+                  </div>
+                  <div className="send-sample-message-section">
+                    Send private photos and messages to a friend or group.
+                  </div>
+                </div>
               )}
-            />
-          ) : null}
+            </div>
+          </div>
         </div>
       );
     };
@@ -89,9 +139,3 @@ export default ConversationsList;
 const findActiveConversation = (conversations, activeConversation) => conversations.find(
   conversation => conversation.id === activeConversation,
 );
-
-const mapConversations = (conversations, handleClick) => conversations.map(conversation => (
-  <div className="p-2 card bg-secondary mb-2 col-md-2" key={conversation.id} onClick={() => handleClick(conversation.id)}>
-    {conversation.title}
-  </div>
-));
