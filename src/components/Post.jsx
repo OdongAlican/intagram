bf6ad0732d872edb9491bd2e8448b65c6d5b8637
OdongAlican <sandieo.2020@gmail.com>
2021-04-06 19:31:31 +0300

    update post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,11 @@ import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { useDispatch } from 'react-redux';
 import { addCommentToPost } from '../actions/comment';
-import { likeToPost } from '../actions/like';
+import {
+  likeToPost, dislikeToPost,
+  bookmarkToPost,
+  disbookmarkToPost,
+} from '../actions/like';
 import Profileimage from '../Images/icon.png';
 import NikeOne from '../Images/nike1.jpg';
 import NikeTwo from '../Images/nike2.jpg';
@@ -13,6 +17,8 @@ import LastSeen from './LastSeen';
 
 const Post = ({ post }) => {
   const images = [NikeOne, NikeTwo, NikeThree];
+  const likesArray = [];
+  const bookmarksArray = [];
   const [content, setContent] = useState('');
   const dispatch = useDispatch();
   const addComment = e => {
@@ -25,6 +31,39 @@ const Post = ({ post }) => {
     e.preventDefault();
     dispatch(likeToPost(post.id));
   };
+
+  const bookmarkPost = e => {
+    e.preventDefault();
+    dispatch(bookmarkToPost(post.id));
+  };
+
+  const dislikePost = e => {
+    e.preventDefault();
+    post.likes.forEach(like => {
+      if (like.post_id === post.id
+        && like.user_id === parseInt(localStorage.userId, 10)) {
+        dispatch(dislikeToPost(like.id));
+      }
+    });
+  };
+
+  const disBookmarkPost = e => {
+    e.preventDefault();
+    post.bookmarks.forEach(bookmark => {
+      if (bookmark.post_id === post.id
+        && bookmark.user_id === parseInt(localStorage.userId, 10)) {
+        dispatch(disbookmarkToPost(bookmark.id));
+      }
+    });
+  };
+
+  post.likes.forEach(post => {
+    likesArray.push(post.user_id);
+  });
+
+  post.bookmarks.forEach(post => {
+    bookmarksArray.push(post.user_id);
+  });
 
   return (
     <div className="single-post-section">
@@ -62,17 +101,34 @@ const Post = ({ post }) => {
       </div>
       <div className="lower-svg-section">
         <div className="like-and-comment-section">
-          <svg
-            onClick={likePost}
-            aria-label="Like"
-            className="_8-yf5 first-post-svg"
-            fill="#262626"
-            height="24"
-            viewBox="0 0 48 48"
-            width="24"
-          >
-            <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z" />
-          </svg>
+
+          {
+            !likesArray.includes(parseInt(localStorage.userId, 10)) ? (
+              <svg
+                onClick={likePost}
+                aria-label="Like"
+                className="_8-yf5 first-post-svg"
+                fill="#262626"
+                height="24"
+                viewBox="0 0 48 48"
+                width="24"
+              >
+                <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z" />
+              </svg>
+            ) : (
+              <svg
+                onClick={dislikePost}
+                aria-label="Like"
+                className="_8-yf5 first-post-svg"
+                height="24"
+                fill="red"
+                viewBox="0 0 48 48"
+                width="24"
+              >
+                <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z" />
+              </svg>
+            )
+          }
           <svg
             aria-label="Comment"
             className="_8-yf5 second-post-svg"
@@ -93,16 +149,34 @@ const Post = ({ post }) => {
           >
             <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z" />
           </svg>
-          <svg
-            aria-label="Save"
-            className="_8-yf5 last-pos-svg"
-            fill="#262626"
-            height="24"
-            viewBox="0 0 48 48"
-            width="24"
-          >
-            <path d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 29 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1zM24 26c.8 0 1.6.3 2.2.9l15.8 16V3H6v39.9l15.8-16c.6-.6 1.4-.9 2.2-.9z" />
-          </svg>
+          {
+          !bookmarksArray.includes(parseInt(localStorage.userId, 10)) ? (
+
+            <svg
+              aria-label="Save"
+              onClick={bookmarkPost}
+              className="_8-yf5 last-pos-svg"
+              fill="#262626"
+              height="24"
+              viewBox="0 0 48 48"
+              width="24"
+            >
+              <path d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 29 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1zM24 26c.8 0 1.6.3 2.2.9l15.8 16V3H6v39.9l15.8-16c.6-.6 1.4-.9 2.2-.9z" />
+            </svg>
+          ) : (
+            <svg
+              aria-label="Save"
+              onClick={disBookmarkPost}
+              className="_8-yf5 last-pos-svg"
+              fill="#262626"
+              height="24"
+              viewBox="0 0 48 48"
+              width="24"
+            >
+              <path d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 28.9 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1z" />
+            </svg>
+          )
+          }
         </div>
         <div className="total-likes-section">
           { (post.likes.length)}
