e647f5675f05c6a912ff0097b63ef6acbd88cef7
OdongAlican <sandieo.2020@gmail.com>
2021-05-04 00:16:06 +0300

    update methods

--- src/actions/like.js
+++ src/actions/like.js
@@ -5,7 +5,7 @@ import {
 } from '../utils/api';
 import { postsSuccessFetch } from './post';
 
-export const likeToPost = (id, token) => async dispatch => {
+export const likeToPost = (id, token, post = null, history) => async dispatch => {
   const method = 'post';
   const path = `/posts/${id}/likes`;
   const data = {
@@ -17,12 +17,15 @@ export const likeToPost = (id, token) => async dispatch => {
     const list = response.data.followeesList;
     const result = { dataList, list };
     dispatch(postsSuccessFetch(result));
+    if (post !== null) {
+      history.push(`/post/${id}`);
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
-export const bookmarkToPost = (id, token) => async dispatch => {
+export const bookmarkToPost = (id, token, post = null, history) => async dispatch => {
   const method = 'post';
   const path = `/posts/${id}/bookmarks`;
   const data = {
@@ -34,12 +37,15 @@ export const bookmarkToPost = (id, token) => async dispatch => {
     const list = response.data.followeesList;
     const result = { dataList, list };
     dispatch(postsSuccessFetch(result));
+    if (post !== null) {
+      history.push(`/post/${id}`);
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
-export const dislikeToPost = (id, token) => async dispatch => {
+export const dislikeToPost = (id, token, post = null, history) => async dispatch => {
   console.log(id, 'inside data first');
   const method = 'delete';
   const path = `/likes/${id}`;
@@ -49,12 +55,16 @@ export const dislikeToPost = (id, token) => async dispatch => {
     const list = response.data.followeesList;
     const result = { dataList, list };
     dispatch(postsSuccessFetch(result));
+    if (post !== null) {
+      console.log('we are here!!');
+      history.push(`/post/${post}`);
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
-export const disbookmarkToPost = (id, token) => async dispatch => {
+export const disbookmarkToPost = (id, token, post = null, history) => async dispatch => {
   const method = 'delete';
   const path = `/bookmarks/${id}`;
   try {
@@ -64,6 +74,10 @@ export const disbookmarkToPost = (id, token) => async dispatch => {
     const list = response.data.followeesList;
     const result = { dataList, list };
     dispatch(postsSuccessFetch(result));
+    if (post !== null) {
+      console.log('we are here!!');
+      history.push(`/post/${post}`);
+    }
   } catch (error) {
     console.log(error);
   }
